246bd5326d4f619e0dc19fa6e09845b07c134106
tianjinzan01 <tianjinzan01@inspur.com>
2022-07-18 18:59:49 +0800

    code update

--- cloud-common/src/main/java/com/tjz/cloudcommon/result/IResultCode.java
+++ cloud-common/src/main/java/com/tjz/cloudcommon/result/IResultCode.java
@@ -2,16 +2,21 @@ package com.tjz.cloudcommon.result;
 
 import java.io.Serializable;
 
+/**
+ * @author 14791
+ */
 public interface IResultCode extends Serializable {
     /**
      * 获取结果消息
+     *
      * @return 结果消息
      */
-    String getMessage();
+    String getMessage ();
 
     /**
-     *  获取返回状态码
+     * 获取返回状态码
+     *
      * @return 结果状态码
      */
-    int getCode();
+    int getCode ();
 }
